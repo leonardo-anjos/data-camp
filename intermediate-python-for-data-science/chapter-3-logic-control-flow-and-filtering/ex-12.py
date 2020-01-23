94d4868ff62352519e1dc2d13d220b2b290512f9
Leonardo Anjos <leonardoanjos@insight.ufc.br>
2020-01-23 12:49:12 -0200

    part of chapter 3

---
+++ intermediate-python-for-data-science/chapter-3-logic-control-flow-and-filtering/ex-12.py
@@ -0,0 +1,16 @@
+# Define variables
+room = "kit"
+area = 14.0
+
+# if-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+else :
+    print("looking around elsewhere.")
+
+# if-else construct for area
+if area > 15 :
+    print("big place!")
+    
+else:
+    print("pretty small.")
