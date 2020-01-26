9a1e1c8e9a148c504f5e23e9c84b11862eab3c6f
Leonardo Anjos <leonardoanjos@insight.ufc.br>
2020-01-26 16:22:52 -0200

    chapter 4 - while lopps

---
+++ intermediate-python-for-data-science/chapter-4-loops/ex-04.py
@@ -0,0 +1,10 @@
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0:
+        offset = offset - 1
+    else:
+        offset = offset + 1
+    print(offset)
