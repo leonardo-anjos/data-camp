895f932d25236a30932ca72fb7bfa94755f8f5df
Leonardo Anjos <leonardoanjos@insight.ufc.br>
2020-02-03 06:34:51 -0200

    part 1 - vhapter 5

---
+++ intermediate-python-for-data-science/chapter-5-case-study-hacker-statistics/ex-05.py
@@ -0,0 +1,12 @@
+
+# Define shout with the parameter, word
+def shout(str):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = str + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout('congratulations')
